8c4073313f1d9297a8c6d7a2c6cfa3af94e8f1c1
BryAintACoder <142674216+BryAintACoder@users.noreply.github.com>
2024-05-30 03:58:55 +0800

    Update configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+# Update apt cache.
+sudo apt-get update
+
+# Install Nginx.
+sudo apt-get install -y nginx
+
+# Set the home page.
+echo "<html><body><h2> My name is $(hostname). Welcome to Azure!.</h2></body></html>" | sudo tee -a /var/www/html/index.html
